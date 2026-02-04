d5d1d7bd39b040d9327da0c6e98f4184ed2fabaf
esotericelf <esoteric.elf@gmail.com>
2026-02-04 23:44:53 +0800

    Fix build error: remove unused Navigate import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Page1 from './Page1';
 import Page2 from './Page2';
 import './App.css';
